ebc6ca5a20e4a1a7cc4ceb6f179cb513ed03e5f4
MatthewM3302 <122694466+MatthewM3302@users.noreply.github.com>
2025-02-17 13:56:36 -0500

    Combined Autonomous routines

--- src/main/java/frc/robot/commands/Autonomous/CenterAndStationPickup.java
+++ src/main/java/frc/robot/commands/Autonomous/CenterAndStationPickup.java
@@ -2,12 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Autonomous.Blue;
+package frc.robot.commands.Autonomous;
 
 import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
@@ -29,7 +30,7 @@ import frc.robot.subsystems.Navigation;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class CenterAndStationPickupBlue extends SequentialCommandGroup {
+public class CenterAndStationPickup extends SequentialCommandGroup {
   /** Creates a new WallSideGroundScoreBlue. */
   DriveSubsystem m_drive;
   Navigation m_nav;
@@ -41,13 +42,15 @@ public class CenterAndStationPickupBlue extends SequentialCommandGroup {
   double position2;
   double poleOffset1;
   double poleOffset2;
-  int aprilTagReef = 20;
-  int aprilTagStation = 13;
-  
+  int aprilTagReef;
+  int aprilTagStation;
+  double stationRightXOffset;
+  double stationRightYOffset;
   Pose2d aprilTagPoseReef;
   Pose2d aprilTagPoseStation;
+  Alliance kAlliance;
 
-  public CenterAndStationPickupBlue(RobotContainer bot, double elevatorScoringPosition1, double elevatorScoringPosition2, boolean leftFirst) {
+  public CenterAndStationPickup(RobotContainer bot, double elevatorScoringPosition1, double elevatorScoringPosition2, boolean leftFirst, String alliance) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     m_drive = bot.m_robotDrive;
@@ -59,6 +62,22 @@ public class CenterAndStationPickupBlue extends SequentialCommandGroup {
     //customisable scoring positions so we dont have to make so many commands
     position1 = elevatorScoringPosition1;
     position2 = elevatorScoringPosition2;
+
+    if (alliance == "Blue" || alliance == "blue"){
+      aprilTagReef = 20;
+      aprilTagStation = 13;
+      stationRightXOffset = Constants.FieldConstants.poseOffsetStationRightX;
+      stationRightYOffset = Constants.FieldConstants.poseOffsetStationRightY;
+    } else {
+      //red
+      aprilTagReef = 11;
+      aprilTagStation = 1;
+      stationRightXOffset = -Constants.FieldConstants.poseOffsetStationRightX;
+      stationRightYOffset = -Constants.FieldConstants.poseOffsetStationRightY;
+    }
+    
+    
+
     //configurable pole positions so we can create distance between us and our teammates if need be instead of going to the same pole first each time we run auto
     if (leftFirst){
       poleOffset1 = Constants.FieldConstants.yOffsetReefPoleLeft;
@@ -67,8 +86,10 @@ public class CenterAndStationPickupBlue extends SequentialCommandGroup {
       poleOffset1 = Constants.FieldConstants.yOffsetReefPoleRight;
       poleOffset2 = Constants.FieldConstants.yOffsetReefPoleLeft;
     }
+    
     aprilTagPoseReef = m_nav.getPose2dInFrontOfTag(aprilTagReef, 1.0);
     aprilTagPoseStation = m_nav.getPose2dInFrontOfTag(aprilTagStation, 2.5);
+
     addCommands(new MoveRobotAndElevator(m_drive, m_nav, m_elevator, aprilTagPoseReef, elevatorScoringPosition1), 
                 new DriveToAprilTag(m_drive, m_nav, aprilTagReef),
                 GoToCommand.relative(m_drive, m_nav, 0.0, poleOffset1, 0.0), //I think this is the only time it makes sense to use relative so we dont have to have coordinates which are different for each side of the reef.
@@ -76,7 +97,7 @@ public class CenterAndStationPickupBlue extends SequentialCommandGroup {
                 new WaitCommand(0.2),
                 new OpenGripper(m_gripper),
                 new WaitCommand(0.2),
-                new MoveRobotAndElevator(m_drive, m_nav, m_elevator, new Pose2d(aprilTagPoseStation.getX() + Constants.FieldConstants.poseOffsetStationRightX, aprilTagPoseStation.getY() + Constants.FieldConstants.poseOffsetStationRightY, aprilTagPoseStation.getRotation()), Constants.ElevatorConstants.kGround),
+                new MoveRobotAndElevator(m_drive, m_nav, m_elevator, new Pose2d(aprilTagPoseStation.getX() + stationRightXOffset, aprilTagPoseStation.getY() + stationRightYOffset, aprilTagPoseStation.getRotation()), Constants.ElevatorConstants.kGround),
                 //the change to the pose is supposed to move the robot so it is facing the right side of the station, to minimize the chances of getting in someones way
                 new AutoCoralPickupGround(m_drive, m_nav, m_intake, 1.0), //just drives forward with intake on, I want something that isn't just guesswork, maybe color detection
                 new IntakeCycle(m_intake, m_intakeArm, m_gripper), //intake cycle does everything from spinning the intake, moving the arm, and transferring from the arm to the gripper, it skips the steps that have already been done
