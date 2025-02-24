ca3fc8849816481925c3ee31da623ff1fd016c8f
Ted Hansen <ted@detroitlabs.com>
2025-02-24 10:54:14 -0500

    rotation fixed, field xy still rotated

--- src/main/java/frc/robot/subsystems/Ludwig/LudwigDriveTrain.java
+++ src/main/java/frc/robot/subsystems/Ludwig/LudwigDriveTrain.java
@@ -18,6 +18,7 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.ADIS16448_IMU.CalibrationTime;
 import edu.wpi.first.wpilibj.ADIS16448_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.simulation.ADIS16448_IMUSim;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.DriveSubsystemBase;
@@ -52,9 +53,11 @@ public class LudwigDriveTrain extends DriveSubsystemBase {
   // The gyro sensor
   // private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
   private final ADIS16448_IMU m_gyro = new ADIS16448_IMU(IMUAxis.kX, SPI.Port.kMXP, CalibrationTime._1s);
+  private final ADIS16448_IMUSim m_gyroSim = new ADIS16448_IMUSim(m_gyro);
   private double m_gyroOffsetDeg = 0.0;
 
   private PIDController headingPidController;
+  private ChassisSpeeds CommandedSpeeds = new ChassisSpeeds();
 
   /** Creates a new DriveSubsystem. */
   public LudwigDriveTrain() {
@@ -65,6 +68,7 @@ public class LudwigDriveTrain extends DriveSubsystemBase {
     headingPidController.setTolerance(2.0);
 
     SmartDashboard.putData("headingPIDcontroller", headingPidController);
+    SmartDashboard.putData("IMU", m_gyro);
 
     // todo add the swerve drive to the dashboard
     SmartDashboard.putData("Swerve Drive", new Sendable() {
@@ -132,12 +136,19 @@ public class LudwigDriveTrain extends DriveSubsystemBase {
   }
 
   public void drive(ChassisSpeeds speeds, Translation2d centerOfRotation) {
+    CommandedSpeeds = speeds;
     var swerveModuleStates = DriveConstants.kinematics.toSwerveModuleStates(speeds, centerOfRotation);
     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, maxSpeedLimit);
     m_frontLeft.setDesiredState(swerveModuleStates[0]);
     m_frontRight.setDesiredState(swerveModuleStates[1]);
     m_rearLeft.setDesiredState(swerveModuleStates[2]);
     m_rearRight.setDesiredState(swerveModuleStates[3]);
+
+    if(Robot.isSimulation()) {
+      double turnRate = speeds.omegaRadiansPerSecond;
+      m_gyroSim.setGyroRateX(-Math.toDegrees(turnRate));
+      m_gyroSim.setGyroAngleX(-Math.toDegrees(turnRate * Robot.kDefaultPeriod) + m_gyro.getAngle());
+    }
   }
 
   /**
@@ -243,6 +254,9 @@ public class LudwigDriveTrain extends DriveSubsystemBase {
     }, (x) -> {
       maxRotationLimit = x;
     });
+    builder.addDoubleProperty("CommandedVx", ()->CommandedSpeeds.vxMetersPerSecond, null);
+    builder.addDoubleProperty("CommandedVy", ()->CommandedSpeeds.vyMetersPerSecond, null);
+    builder.addDoubleProperty("CommandedOmega", ()->CommandedSpeeds.omegaRadiansPerSecond, null);
   }
 
   @Override
