a711362377ff9ad23ad8b013744304d1f47ab3a7
Ted Hansen <ted_hansen@bose.com>
2025-02-26 13:05:48 -0500

    add arm simulation

--- src/main/java/frc/robot/subsystems/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/IntakeArm.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXSimCollection;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.math.MathUtil;
@@ -13,9 +14,12 @@ import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.simulation.DutyCycleEncoderSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
@@ -24,7 +28,9 @@ import frc.robot.Robot;
 public class IntakeArm extends SubsystemBase {
 
   private VictorSPX m_armSpx;
+  private VictorSPXSimCollection m_armSpxSim;
   private DutyCycleEncoder m_ArmEncoder;
+  private DutyCycleEncoderSim m_ArmEncoderSim;
   private double m_armAngleOffset = IntakeConstants.armAngleOffset;
   public ProfiledPIDController m_PidController;
   private ArmFeedforward m_Feedforward;
@@ -34,16 +40,24 @@ public class IntakeArm extends SubsystemBase {
   private double kA = IntakeConstants.kA;
   private double kMaxArmAngle = IntakeConstants.MaxArmAngle;
   private double kMinArmAngle = IntakeConstants.MinArmAngle;
+  private final double kGearRatio = 100.0;
+  
+  // simulation constants
+  private final double kMoment = 32.3; //kg-m^2
+  private final double kArmLength = .355;
 
   private LinearFilter m_velocityFilter;
   private double m_lastArmAngle;
   private double m_armVelocity = 0.0;
+
+  private SingleJointedArmSim m_sim;
 
   /** Creates a new IntakeArm. */
   public IntakeArm() {
     m_armSpx = new VictorSPX(Constants.CanIds.intakeArmMotorID);
     m_armSpx.setNeutralMode(NeutralMode.Brake);
     m_armSpx.setInverted(true);
+    m_armSpxSim = m_armSpx.getSimCollection();
     m_ArmEncoder = new DutyCycleEncoder(IntakeConstants.armEncoderDInput);
     m_ArmEncoder.setDutyCycleRange(1.0 / 1025.0, 1024.0 / 1025.0);
     m_PidController = new ProfiledPIDController(IntakeConstants.kP, IntakeConstants.kI, IntakeConstants.kD, 
@@ -51,6 +65,15 @@ public class IntakeArm extends SubsystemBase {
     resetFeedForward();
     m_velocityFilter = LinearFilter.singlePoleIIR(0.1, Robot.kDefaultPeriod);
     m_lastArmAngle = getArmAngleDegrees();
+    m_sim = new SingleJointedArmSim(DCMotor.getAndymarkRs775_125(1),
+                                    kGearRatio,
+                                    kMoment,
+                                    kArmLength,
+                                    Math.toRadians(kMinArmAngle),
+                                    Math.toRadians(kMaxArmAngle),
+                                    true,
+                                    Math.toRadians(kMaxArmAngle)
+                                    );
   }
 
   private void resetFeedForward() {
@@ -121,5 +144,12 @@ public class IntakeArm extends SubsystemBase {
     builder.addDoubleProperty("kMaxArmAngle", () -> kMaxArmAngle, (x) -> {
       kMaxArmAngle = x;
     });
+  }
+
+  @Override
+  public void simulationPeriodic(){
+    m_sim.setInputVoltage(m_armSpxSim.getMotorOutputLeadVoltage());
+    m_sim.update(Robot.kDefaultPeriod);
+    m_ArmEncoderSim.set(m_sim.getAngleRads()/6.28318);
   }
 }
